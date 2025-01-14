6e8b3d325b3e30b8fc6a355653a243e3b37ce5d5
sayem499 <sayem499@gmail.com>
2025-01-15 00:40:50 +0600

    added hamburger button next to logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,14 +22,29 @@ const Navbar = () => {
       }`}
     >
       <div className="container mx-auto max-w-screen-xl flex justify-between items-center">
-      <div className="">
+      <div className="flex items-center gap-2">
+      <svg
+        stroke="currentColor"
+        fill="currentColor"
+        strokeWidth="0"
+        viewBox="0 0 512 512"
+        color={`${
+            scrolled ? '#1dc468' : '#ffff'
+          }`}
+        style={{ cursor: 'pointer' }}
+        height="20"
+        width="20"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M64 384h384v-42.67H64zm0-106.67h384v-42.66H64zM64 128v42.67h384V128z"></path>
+    </svg>
           <a href='/' className='flex items-center font-sans' alt='AlterYouth'>
             <SvgAlteryouthLogo color={`${
         scrolled ? '#1dc468' : '#ffff'
-      }`} style={{ width: '12em', height: '4em' }} /> {/* color="#1dc468" */}
+      }`} style={{ width: '12em', height: '4em', fontFamily: 'sans-serif' }} /> {/* color="#1dc468" */}
           </a>
         </div>
-        <ul className={`flex space-x-4 ${scrolled ? 'text-[#1dc468]' : 'text-[#ffff]'}`}>
+        <ul className={`flex space-x-2 gap-4 ${scrolled ? 'text-[#1dc468]' : 'text-[#ffff]'}`}>
           {['About', 'How It Works', 'Scholarships', 'Collaborate', 'Login'].map((link) => (
             <li key={link} className="relative font-bold text-sm">
               <a
