481a6e5556f1effa33106c8effe70d8a16809e24
sayem499 <sayem499@gmail.com>
2025-01-19 11:47:55 +0600

    Fixed design for app mockup section.

--- src/components/PhoneMockup.jsx
+++ src/components/PhoneMockup.jsx
@@ -2,16 +2,16 @@ import Image from 'next/image';
 
 const AlterYouthHero = () => {
   return (
-    <section className="relative mb-10 mt-4 pb-5 h-full w-full">
+    <section className="relative mb-10 mt-2 pb-5 h-full w-full">
       <div className="absolute w-full inset-0 z-0 h-full "></div>
       <div className="container mx-auto px-4 h-full">
-        <div className="flex flex-col items-center h-full mt-10">
-          <h1 className="text-center text-3xl lg:text-3xl font-bold">The Uber for Scholarships</h1>
-          <p className="text-center text-sm">
+        <div className="flex flex-col items-center h-full mt-2">
+          <h1 className="text-center text-3xl lg:text-3xl font-bold mt-4">The Uber for Scholarships</h1>
+          <p className="text-center text-base mt-4">
             A platform connecting the world with financially struggling students<br />
             in Government Primary Schools of Bangladesh
           </p>
-          <div className="flex space-x-4 justify-center">
+          <div className="flex space-x-4 justify-center mt-4">
             <a
               href="https://apps.apple.com/us/app/alteryouth-start-scholarships/id1569106859"
               target="_blank"
@@ -37,15 +37,20 @@ const AlterYouthHero = () => {
               />
             </a>
           </div>
-          <div className="flex flex-col w-full lg:flex-row items-center justify-evenly mt-24">{/* gap-6 lg:gap-16 */}
-            <div className="text-center">
+          <div className="flex flex-col w-full lg:flex-row items-center justify-evenly mt-16">{/* gap-6 lg:gap-16 */}
+            <div className="flex flex-col justify-center text-center">
               <Image
                 src="/images/app_scholarship.jpg"
                 alt="Monthly scholarship transfer details in the AlterYouth app"
                 width={180}
                 height={200}
+                className='mx-auto'
               />
-              <h3 className="mt-4 font-semibold">Monthly Scholarship Transfer Details</h3>
+                <div className='mt-4'>
+                  <h3 className="font-bold">Transfer directly</h3>
+                  <p className='w-[350px] mt-3 text-sm'>Your scholarship is transferred directly to your<br/> student's family on their mobile bank account.</p>
+                </div>
+              
             </div>
             <div className="text-center">
               <Image
@@ -53,8 +58,13 @@ const AlterYouthHero = () => {
                 alt="School report updates in the AlterYouth app"
                 width={180}
                 height={200}
+                className='mx-auto'
               />
-              <h3 className="mt-4 font-semibold">School Report Updates</h3>
+                <div className='mt-4'>
+                  <h3 className="font-bold">Get school reports</h3>
+                  <p className='w-[350px] mt-3 text-sm'>View attendance data and report cards from your<br/> student's school, until completion of Class 5.</p>
+                </div>
+              
             </div>
             <div className="text-center">
               <Image
@@ -62,8 +72,13 @@ const AlterYouthHero = () => {
                 alt="School report updates in the AlterYouth app"
                 width={180}
                 height={200}
+                className='mx-auto'
               />
-              <h3 className="mt-4 font-semibold">School Report Updates</h3>
+              <div className='mt-4'>
+                <h3 className="mt-4 font-semibold">Ensure a literate citizen</h3>
+                <p className='w-[350px] mt-3 text-sm'>Your scholarship continues until completion of<br/> class 5, when your student achieves literacy.</p>
+              </div>
+              
             </div>
           </div>
         </div>
